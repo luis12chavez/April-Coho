54f9b4ef59094a51978fec4dd499729ff612443e
Luis Chavez <luis12chavez@gmail.com>
2024-05-07 12:33:01 -0700

    .

---
+++ WK-2/Building a Linked List/notes.js
@@ -0,0 +1,15 @@
+/*********************************************************************************************************************************/
+class Node {
+    constructor(data) {
+        // data and the Pointer to the next node will be setup as attributes
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
+
