58bfa7a32ac068ce1913d86dd483de3564f77692
Luis Chavez <luis12chavez@gmail.com>
2024-05-07 13:06:42 -0700

    test

--- WK-2/Building a Linked List/notes.js
+++ WK-2/Building a Linked List/notes.js
@@ -11,5 +11,30 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+/*********************************************************************************************************************************/
+// Building Linked List Methods
+addFront(val) {
+    // Creating a new node object with the value provided
+    let new_node = new Node(val);
+    // Checking to see if the current list does not have a head node (if the list is empty)
+    // If the list is empty, place the new node as the head 
+    if (!this.head) {
+        this.head = new_node;
+        return this;
+    }
+    // If the list is not empty, assign the head to be the next node to the new node (Blue Arrow in picture above)
+    new_node.next = this.head;
+    // Then finally assign the new_node to be the new head of the list (Red Arrow in picture above)
+    this.head = new_node;
+    return this;
 }
+}
+
+// let newNode = new Node([1,2,3,4,5])
+// console.log(newNode)
+// let newLinkedList =  new LinkedList()
+// // console.log(newLinkedList.addFront(newNode))
+// newLinkedList.addFront(newNode)
+// console.log(newLinkedList)
 
